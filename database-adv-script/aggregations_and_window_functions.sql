73344aed97e38c6582839fd511e380d087319518
fadhili5 <denisfadhili5@gmail.com>
2025-07-05 08:15:05 +0300

    added queries to demonstrate SQL aggregation and window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,16 @@
+-- USING SQL AGGREGATION AND WINDOW FUNCTIONS TO ANALYZE DATA
+
+
+-- Query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause
+SELECT user_id, COUNT(booking_id) AS total_booking
+FROM Booking
+GROUP BY user_id;
+
+
+-- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+SELECT property_id, total_bookings, ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number_rank
+FROM(
+    SELECT property_id, COUNT(booking_id) AS total_bookings
+    FROM Booking
+    GROUP BY property_id
+) AS property_bookings;

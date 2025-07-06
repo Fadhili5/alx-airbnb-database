bba4cce66e077f0ac6ceb7716af991c8c5ce1fff
fadhili5 <denisfadhili5@gmail.com>
2025-07-06 11:25:10 +0300

    added WHERE n AND clause in the query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -24,7 +24,7 @@ JOIN Property p ON b.property_id = p.property_id
 JOIN Payment pay ON b.booking_id = pay.booking_id;
 
 
---Analyzing query's performance using EXPLAIN and identify any inefficiencies
+-- Analyzing query's performance using EXPLAIN and identify any inefficiencies
 EXPLAIN SELECT
     b.booking_id,
     b.property_id,
@@ -55,3 +55,22 @@ LEFT JOIN Payment pay ON b.booking_id = pay.booking_id; -- includes bookings tha
 CREATE INDEX IF NOT EXISTS idx_booking_user_id ON Booking(user_id);
 CREATE INDEX IF NOT EXISTS idx_booking_property_id ON Booking(property_id);
 CREATE INDEX IF NOT EXISTS idx_payment_booking_id ON Payment(booking_id);
+
+-- Final query which is optimized
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.payment_method
+FROM Booking b
+JOIN "User" u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+    AND b.total_price > 0
+LIMIT 100;
